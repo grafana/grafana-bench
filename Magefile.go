5755301790fb8e422b9054a504399ff6e4ff3672
Jeff Levin <jeff@levinology.com>
2023-04-12 13:43:54 -0800

    checkpoint converting to magefile. mostly untests

---
+++ Magefile.go
@@ -0,0 +1,266 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/magefile/mage/sh"
+)
+
+var (
+	commit  string = "main"
+	workdir string = getWorkdir()
+
+	sys_os        string
+	sys_arch      string
+	arch          string
+	artifact_name string
+	artifact_path string
+)
+
+// Resolve branch to latest commit of branch
+func ResolveCommit() error {
+	fmt.Println("grafana commit", commit)
+	if commit == "main" {
+		resolved, err := sh.Output("git", "ls-remote", "https://github.com/grafana/grafana", "HEAD", "-c7")
+		if err != nil {
+			return fmt.Errorf("Error resolving git commit %s: %s", commit, err)
+		}
+
+		resolved = strings.Split(resolved, "\t")[0]
+
+		fmt.Printf("grafana commit `%s` resolved to `%s`", commit, resolved)
+		commit = resolved
+	}
+	return nil
+}
+
+// Resolve architecture and artifact names
+func ResolveArch() error {
+	if arch == "" {
+		sys_os, err := sh.Output("uname -s")
+		if err != nil {
+			return fmt.Errorf("error resolving OS %s", err)
+		}
+
+		sys_arch, err := sh.Output("uname -m")
+		if err != nil {
+			return fmt.Errorf("error resolve architecture %s", err)
+		}
+		arch = fmt.Sprintf("%s/%s", strings.ToLower(sys_os), strings.ToLower(sys_arch))
+	}
+
+	artifact_name = fmt.Sprintf("grafana-server-%s-%s", commit, strings.Replace(arch, "/", "-", -1))
+	artifact_path = path.Join("workdir", artifact_name)
+	return nil
+}
+
+// Clone test and build repos locally
+func Bootstrap() error {
+
+	// check if test repo is cloned locally
+	exists, err := pathExists("tests")
+	if err != nil {
+		return fmt.Errorf("Issue checking directory path")
+	}
+
+	if !exists {
+		if err := sh.RunV("git", "clone", "https://github.com/grafana/grafana-api-tests", "tests"); err != nil {
+			return fmt.Errorf("Error checking out grafana test repo %s", err)
+		}
+	}
+
+	// check if build repo cloned locally
+	exists, err = pathExists("build")
+	if err != nil {
+		return fmt.Errorf("Issue checking directory path")
+	}
+
+	if !exists {
+		if err := sh.RunV("git", "clone", "https://github.com/grafana/grafana-build", "build"); err != nil {
+			return fmt.Errorf("Error checking out grafana test repo %s", err)
+		}
+	}
+
+	// get grafana test repo
+	// update it
+	return nil
+}
+
+// Update test and clone repos
+func Update() error {
+	// tests
+	err := doInDir(workdir, "tests", func() error {
+		if err := sh.RunV("git", "checkout", "main"); err != nil {
+			return fmt.Errorf("Error checking out grafana test repo %s", err)
+		}
+
+		if err := sh.RunV("git", "pull"); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	// build
+	err = doInDir(workdir, "build", func() error {
+		if err := sh.RunV("git", "checkout", "main"); err != nil {
+			return fmt.Errorf("Error checking out grafana test repo %s", err)
+		}
+
+		if err := sh.RunV("git", "pull"); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	return err
+}
+
+// Sets necessary dependencies before real operations
+func SetDependencies() error {
+	err := ResolveCommit()
+	if err != nil {
+		return err
+	}
+	err = ResolveArch()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func BuildCommit() error {
+	err := SetDependencies()
+	if err != nil {
+		return err
+	}
+
+	exists, _ := pathExists(artifact_path)
+	if exists {
+		fmt.Println("build artifact exists, skipping:", artifact_path)
+	}
+
+	// do the build
+	err = doInDir(workdir, "build", func() error {
+		ref := fmt.Sprintf("--grafana-ref=%s", commit)
+		distro := fmt.Sprintf("--distro=%s", arch)
+		err := sh.RunV("go", "run", "./cmd", "--verbose", ref, "backend", "build", distro)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+
+	// copy build to artifact path
+	// artifacts grafana, grafana-server, grafana-cli
+	buildPath := path.Join(workdir, "build", "bin", arch, "grafana")
+	if err := sh.RunV("cp", buildPath, artifact_path); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func BootCommit() error {
+	err := SetDependencies()
+	if err != nil {
+		return err
+	}
+
+	// create working directory
+	workPath := path.Join(workdir, "work")
+	if err := sh.RunV("mkdir", "-p", workPath); err != nil {
+		return err
+	}
+
+	// make sure working directory is empty
+	workPathFiles := fmt.Sprintf("%s/*", workPath)
+	if err := sh.RunV("rm", "-r", workPathFiles); err != nil {
+		return err
+	}
+
+	// copy conf directory
+	confPath := path.Join(workdir, "conf")
+	workConfPath := path.Join(workPath, "conf")
+	if err := sh.RunV("cp", "-r", confPath, workConfPath); err != nil {
+		return err
+	}
+
+	// get default.ini for that commit
+	iniUrl := fmt.Sprintf("https://raw.githubusercontent.com/grafana/grafana/$(COMMIT)/conf/defaults.ini")
+	iniDest := path.Join(workdir, "work", "conf", "defaults.ini")
+	if err := sh.RunV("curl", "-s", "-o", iniUrl, iniDest); err != nil {
+		return err
+	}
+
+	// copy artifact
+	workExecutable := path.Join(workPath, "grafana")
+	if err := sh.RunV("cp", artifact_path, workExecutable); err != nil {
+		return err
+	}
+
+	// TODO
+	// start here
+	// verify build commit works
+	// verify boot commit does everything but boot grafana
+
+	// Run grafana
+	//err = doInDir(workdir, workPath, func() error {
+	//if err := sh.RunV(); err != nil {
+	//return err
+	//}
+	//return nil
+	//})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Do function in a directory
+func doInDir(workdir string, operationDir string, fn func() error) error {
+	os.Chdir(operationDir)
+	defer os.Chdir(workdir)
+	return fn()
+}
+
+// Checks for existence of directory
+func pathExists(path string) (bool, error) {
+	// Use the Stat function to check if the directory exists
+	_, err := os.Stat(path)
+
+	// If there is no error, the path exists
+	if err == nil {
+		return true, nil
+	}
+
+	// If the error is "not exists", the directory does not exist
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	// If the error is not "not exists", return the error
+	return false, err
+}
+
+// Get working directory
+func getWorkdir() string {
+	// Use the Getwd function to get the current working directory
+	dir, err := os.Getwd()
+
+	// If there was an error, return it
+	if err != nil {
+		panic(err)
+	}
+
+	// Otherwise, return the current working directory
+	return dir
+}
