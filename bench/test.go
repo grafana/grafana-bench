12e34b70cac07d88d6f48854776eae461cfd6dc1
Jeff Levin <jeff@levinology.com>
2023-05-01 22:19:42 -0800

    cleanup + docs

--- bench/test.go
+++ bench/test.go
@@ -1,8 +1,6 @@
 package bench
 
 import (
-	"fmt"
-
 	"github.com/grafana/grafana-bench/bench/utils"
 	"github.com/magefile/mage/sh"
 )
@@ -10,33 +8,44 @@ import (
 // Run tests runs test on an instance of grafana already available at port 3000.
 // This does not manage booting or stopping the instance
 func (b *Config) Test() error {
-	// get featureToggles & buildInfo from response /api/frontend/settings
-	// only contains list of things that are turned on
-	liveConfig, err := getLiveConfig()
-	if err != nil {
-		fmt.Println("error getting live config from booted grafana:", err)
-	} else {
-		fmt.Println(liveConfig)
+	if err := b.ResolveConfig(); err != nil {
+		return err
 	}
 
+	if err := b.ResolveTestSuite(); err != nil {
+		return err
+	}
+
+	// Wait for the server to start up
+	waitForLiveGrafana()
+
+	// get featureToggles & buildInfo from response /api/frontend/settings
+	// only contains list of things that are turned on
+	//liveConfig, err := getLiveConfig()
+	//if err != nil {
+	//  fmt.Println("error getting live config from booted grafana:", err)
+	//} else {
+	//  fmt.Println(liveConfig)
+	//}
+
 	// run k6 tests
-	err = utils.DoInDir(b.ProjectRoot, "tests", func() error {
+	err := utils.DoInDir(b.ProjectRoot, "tests", func() error {
 
 		envVars := make(map[string]string)
-		envVars["machineSpec"] = getMachineSpec()
+		envVars["MACHINE_SPEC"] = getMachineSpec()
+		envVars["TEST_SUITE_REVISION"] = b.TestSuiteVersion
 
-		// TODO:
-		// 1. start here. see if we can get these environment variables passed in
-		// correctly
-		// 2. then verify environment variables and test suite information output
+		// TODO: START HERE
 		// 3. change references of "commit" when referring to test suite or build
 		// suite and use "revision"
 		// 4. start thinking about collection results from test suite
+		// file output should come from an env variable but have a default for all
+		// tests
 
 		tests := getTestSuiteFiles()
 		for _, testFile := range tests {
 			// k6 run tests/tests/dashboards.js
-			if err := sh.RunWithV(envVars, "k6", "run", testFile); err != nil {
+			if err := sh.RunWithV(envVars, "k6", "run", testFile, "-i", "1", "-u", "1"); err != nil {
 				return err
 			}
 		}
@@ -54,10 +63,11 @@ func (b *Config) Test() error {
 // e.g. TestFiles=dashboards will get all files in tests/tests/dashboards/**.*.js
 // e.g. TestFiles=dashboards/dashboard_read.js will only run dashboard_read.js
 func getTestSuiteFiles() []string {
-	tests := []string{"tests/dashboards/dashboard_create.js"}
+	//tests := []string{"tests/dashboards/dashboard_update.js"}
+	tests := []string{"tests/summary.js"}
 	return tests
 }
 
 func getMachineSpec() string {
-	return "local|m1max|65536/3.2 GHz|arm64|darwin"
+	return "local|m1max|65536|3.2 GHz|arm64|darwin"
 }
