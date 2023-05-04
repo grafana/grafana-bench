94bcf80b6be31f72bc49fc82d143641b56ab90ab
Jeff Levin <jeff@levinology.com>
2023-05-04 13:10:56 -0800

    new method to resolve commits

--- bench/utils/git.go
+++ bench/utils/git.go
@@ -1,11 +1,26 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/magefile/mage/sh"
 )
+
+// response for commit info
+type GithubCommitResponse struct {
+	Sha string `json:"sha"`
+}
+
+// response for branch info
+type GithubBranchResponse struct {
+	Commit struct {
+		Sha string `json:"sha"`
+	} `json:"commit"`
+}
 
 // determines when string is a branch or commit
 func IsCommitHash(branchOrCommit string) bool {
@@ -32,4 +47,76 @@ func GetLatestBranchCommit(repo, branch string) (string, error) {
 	}
 
 	return commit, nil
+}
+
+// ResolveCommitSha uses github api to get the full length commit sha from a
+// shorter sha
+func ResolveCommitFullSha(repo, commit string) (string, error) {
+	client := &http.Client{}
+
+	url := fmt.Sprintf("https://api.github.com/repos/%s/commits/%s", repo, commit)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	gr := &GithubCommitResponse{}
+
+	err = json.Unmarshal(body, gr)
+	if err != nil {
+		return "", err
+	}
+
+	return gr.Sha, nil
+}
+
+// ResolveLatestBranchCommit uses github api to return the latest commit sha on
+// that branch
+func ResolveLatestBranchCommit(repo, commit string) (string, error) {
+	client := &http.Client{}
+
+	url := fmt.Sprintf("https://api.github.com/repos/%s/branches/%s", repo, commit)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	gr := &GithubBranchResponse{}
+
+	err = json.Unmarshal(body, gr)
+	if err != nil {
+		return "", err
+	}
+
+	return gr.Commit.Sha, nil
 }
