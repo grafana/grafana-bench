6b586d1f10dd6e6b46486c3150df3836bd51211f
Jeff Levin <jeff@levinology.com>
2023-05-30 16:00:47 -0800

    add bucket for vm state

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,10 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+locals {
+  service_account = "serviceAccount:infra-manager@grafana-bench.iam.gserviceaccount.com"
+}
+
 // create bucket to store builds
 resource "google_storage_bucket" "bench_builds" {
   name          = "bench-builds"
@@ -32,11 +36,45 @@ resource "google_storage_bucket_iam_binding" "build_storage_owner" {
   bucket = google_storage_bucket.bench_builds.name
   role = "roles/storage.admin"
   members = [
-    "serviceAccount:infra-manager@grafana-bench.iam.gserviceaccount.com",
+    local.service_account,
   ]
 }
 
 
+// Create bucket to store VM state
+resource "google_storage_bucket" "bench_vms" {
+  name          = "bench-vms"
+  location      = "US"
+  uniform_bucket_level_access = true
+
+  // don't allow deleting the bucket once we've started putting
+  // builds in it
+  force_destroy = false
+
+  // delete aborted uploads after 1 day
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+
+  // enable versioning in case state gets deleted
+  versioning {
+    enabled = true
+  }
+}
+
+// Assign self as admin
+resource "google_storage_bucket_iam_binding" "vm_state_owner" {
+  bucket = google_storage_bucket.bench_vms.name
+  role = "roles/storage.admin"
+  members = [
+    local.service_account,
+  ]
+}
 
 
 
