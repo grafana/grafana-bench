e3a73db65b72fa73e2a9d0afd3bf1436158aa2f9
Kevin Minehart <kmineh0151@gmail.com>
2023-08-09 13:03:50 -0500

    Simplify logging of a subprocess

--- bench/utils/cmd.go
+++ bench/utils/cmd.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -19,45 +17,12 @@ func ExecStdoutWithEnv(cmd *exec.Cmd, env map[string]string) error {
 
 // ExecStdout runs a command and streams stdout+stderr to the terminal.
 func ExecStdout(cmd *exec.Cmd) error {
-
-	// Set up stderr pipe
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return fmt.Errorf("Error creating stderr pipe: %w", err)
-	}
-
-	// Set up stdout pipe
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("Error creating stdout pipe: %w", err)
-	}
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
 
 	// Start the command
-	err = cmd.Start()
-	if err != nil {
-		return fmt.Errorf("Error starting command: %w", err)
-	}
-
-	// Copy the output to stdout in real-time
-	go func() {
-		_, err := io.Copy(os.Stdout, stdout)
-		if err != nil {
-			log.Println("Error outputting to stdout:", err)
-		}
-	}()
-
-	// Copy the output to stderr in real-time
-	go func() {
-		_, err := io.Copy(os.Stderr, stderr)
-		if err != nil {
-			log.Println("Error outputting to stdout:", err)
-		}
-	}()
-
-	// Wait for the command to finish
-	err = cmd.Wait()
-	if err != nil {
-		return fmt.Errorf("Error waiting for command: %w", err)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error starting command: %w", err)
 	}
 
 	return nil
