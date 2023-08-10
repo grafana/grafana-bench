055e207179a09d4c9c9a3f181f6aa1378a22f1e2
Jeff Levin <jeff@levinology.com>
2023-08-10 08:20:19 -0800

    cleanup

--- docker_startup.sh
+++ docker_startup.sh
@@ -3,9 +3,12 @@ mkdir creds
 
 echo $GCP_KEY_BASE64 | base64 -d >> creds/GCP-infra-manager-828bbfa6f427.json
 
-K6_CLOUD_TOKEN=$(echo $K6_CLOUD_TOKEN | base64 -d)
-echo $K6_CLOUD_TOKEN
+#echo $K6_CLOUD_TOKEN
 echo $K6_CLOUD_PROJECT_ID
+
+echo $GRAFANA_URL
+echo $GRAFANA_USER
+#echo $GRAFANA_PASSWORD
 
 # execute
 grafana-bench $GRAFANA_URL $GRAFANA_PORT $GRAFANA_USER $GRAFANA_PASSWORD $GRAFANA_TEST_SUITE
