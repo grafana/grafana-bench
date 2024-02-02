f52536bef7d04c4ab50ce762d717a9067970b76d
Pablo Chacin <pablochacin@gmail.com>
2024-02-02 16:25:55 +0100

    extract root command into package
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- bench.go
+++ bench.go
@@ -2,62 +2,20 @@ package main
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"os"
-
-	"github.com/grafana/grafana-bench/cmd/runner"
-	"github.com/joho/godotenv"
-	"github.com/spf13/cobra"
-
 	"log/slog"
+
+	"github.com/grafana/grafana-bench/cmd/root"
 )
 
 func main() {
 	log := slog.New(slog.NewTextHandler(os.Stderr, nil))
 
-	root := newRootCmd()
-	root.AddCommand(runner.NewTestRunnerCommand(log))
+	root := root.NewCmd(log)
 
 	err := root.ExecuteContext(context.Background())
 	if err != nil {
 		log.Error(err.Error())
 		os.Exit(1)
 	}
-}	
-
-func newRootCmd() *cobra.Command {
-	var (
-		envFile string
-	)
-
-	rootCmd := &cobra.Command{
-		Use:   "bench",
-		Short: "grafana bench",
-		Long: "bench provides a CLI interface for executing diverse actions for running tests",
-		// prevent the usage help to printed to stderr when an error is reported by a subcommand
-		SilenceUsage:  true,
-		// this is needed to prevent cobra to print errors reported by subcommands in the stderror
-		SilenceErrors: true,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			err := godotenv.Load(envFile)
-
-			// there was an error loading the .env file
-			// do not report if it a "file not found" error for the default value
-			if err != nil {
-				if envFile == "" && errors.Is(err, os.ErrNotExist) {
-					return nil
-				}
-
-				return fmt.Errorf("loading .env file: %w", err)
-			}
-
-			return nil
-		},
-	}
-
-	rootCmd.PersistentFlags().StringVar(&envFile, "env", "", "path to a file with the environment variables." +
-		"\nIf none is specified and a .env files exists in the work directory, it will be used")
-
-	return rootCmd
 }
---
+++ cmd/root/root.go
@@ -0,0 +1,51 @@
+package root
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/grafana/grafana-bench/cmd/runner"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+// NewCmd returns a cobra.Command for grafana bench command
+func NewCmd(log *slog.Logger) *cobra.Command {
+	var (
+		envFile string
+	)
+
+	rootCmd := &cobra.Command{
+		Use:   "bench",
+		Short: "grafana bench",
+		Long:  "bench provides a CLI interface for executing diverse actions for running tests",
+		// prevent the usage help to printed to stderr when an error is reported by a subcommand
+		SilenceUsage: true,
+		// this is needed to prevent cobra to print errors reported by subcommands in the stderror
+		SilenceErrors: true,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			err := godotenv.Load(envFile)
+
+			// there was an error loading the .env file
+			// do not report if it a "file not found" error for the default value
+			if err != nil {
+				if envFile == "" && errors.Is(err, os.ErrNotExist) {
+					return nil
+				}
+
+				return fmt.Errorf("loading .env file: %w", err)
+			}
+
+			return nil
+		},
+	}
+
+	rootCmd.PersistentFlags().StringVar(&envFile, "env", "", "path to a file with the environment variables."+
+		"\nIf none is specified and a .env files exists in the work directory, it will be used")
+
+	rootCmd.AddCommand(runner.NewTestRunnerCommand(log))
+
+	return rootCmd
+}
