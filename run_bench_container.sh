fe25c672fbc8ed3ea10d8832b5a86b6407fcfab1
pablochacin <pablochacin@gmail.com>
2024-02-26 19:13:25 +0100

    Fix run bech container script (#180)
    * add test suite base parameter
    * improve .env file handling
    
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- run_bench_container.sh
+++ run_bench_container.sh
@@ -3,18 +3,14 @@
 # This script is used for testing and iterating on Bench from inside a container.
 # ./run_bech_container.sh dashboards/dashboard_create.js .env
 #
-# 1. the first argument is path to folder or test file. the file is relative
-#    to the volume mounted in the container. 
-#    bench binary is located at   /home/bench
-#    we assume local tests at     $(PWD)/work/test/suite/dist/tests
-#    mounted to                   /home/bench/tests
-#    so test path would be        `tests/path/to/my/test.js`
-#
-# 2. the second is path to .env file. see below for defaults
+# 1. the first argument is the path to base folder to find test suites
+#    mounted in the container to  /home/bench/tests.
+# 2. the test suite folder or test file, relative to test suite base (see above)
+# 3. the third (optional) is path to .env file.
 
 # source the .env file
-if [ "$#" -eq 0 ]; then
-  echo "test file is required"
+if [ "$#" -lt 2]; then
+  echo "base dir and test suite are required"
   exit 1
 fi
 
@@ -27,20 +23,28 @@ else
 fi
 
 # source the .env file
-if [ "$#" -eq 2 ]; then
-  . "$2"
+if [ "$#" -eq 3 ]; then
+  DOTENV=$(readlink -e "$3")
+  if [ -z "$DOTENV" ]; then 
+    echo "env file not found"
+    exit 1
+  fi
 else
-  . .env
+  DOTENV=$(readlink -e .env)
+fi
+
+if [ ! -z "$DOTENV" ]; then
+  DOTENV_VOLUME="--volume=$DOTENV:/home/bench/.env"
 fi
 
 # location of tests on disk
-TEST_LOCATION="$PWD/work/test/suite/dist/tests"
+TESTSUITE_BASE=$(readlink -e $1)
 
 docker run --rm \
   --network host \
   --platform=linux/amd64 \
-  --volume="$TEST_LOCATION:/home/bench/tests" \
+  $DOTENV_VOLUME --volume="$TESTSUITE_BASE:/home/bench/tests" \
   grafana-bench-dev test \
     --test-type="smoke" \
     --test-suite-base="tests" \
-    --test-suite "$1"
+    --test-suite "$2"
