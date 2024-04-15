4da7825503016e088d0612ff78a9745f2d0db53f
pablochacin <pablochacin@gmail.com>
2024-04-15 11:31:24 +0200

    Override grafana url from env variable (#244)
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- cmd/test/command.go
+++ cmd/test/command.go
@@ -95,7 +95,8 @@ func NewCmd(log *slog.Logger) *cobra.Command {
 				benchRevision = env.EnvOrDefault("BENCH_REVISION", revision.BenchRevision())
 			}
 
-			// override grafana user and password from environment variables if they are set
+			// override grafana parameters from environment variables if they are set
+			grafanaUrl = env.EnvOrDefault("GRAFANA_URL", grafanaUrl)
 			grafanaUsername = env.EnvOrDefault("GRAFANA_USER", grafanaUsername)
 			grafanaPassword = env.EnvOrDefault("GRAFANA_PASSWORD", grafanaPassword)
 
@@ -170,7 +171,12 @@ func NewCmd(log *slog.Logger) *cobra.Command {
 	fs := cmd.Flags()
 	fs.StringVar(&testTrigger, "test-trigger", "local", "test trigger")
 	fs.StringVar(&testType, "test-type", "smoke", "test type. Allowed values: 'smoke', 'load'")
-	fs.StringVar(&grafanaUrl, "grafana-url", "http://localhost:3000", "url to grafana instance")
+	fs.StringVar(
+		&grafanaUrl,
+		"grafana-url",
+		"http://localhost:3000",
+		"url to grafana instance. Overridden by the GRAFANA_URL environment variable",
+	)
 	fs.DurationVar(
 		&grafanaTimeout,
 		"grafana-timeout",
