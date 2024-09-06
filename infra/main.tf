6c33f4ed9a3f15e2e651b2a77f9cb40e72530041
Jeff Levin <jeff@levinology.com>
2024-09-06 15:46:17 -0800

    cleanup terraform. bump docs

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = "1.5.7"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+        version = "2.5.1"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.4.2"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "5.33.0"
+    }
+  }
+}
+
 provider "google" {
   credentials = file("../creds/GCP-infra-manager-828bbfa6f427.json")
   project = local.project_id
