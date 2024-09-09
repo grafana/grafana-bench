b656df56d486c7a0c9ce3a4d1e6d11fe3fc816ee
Jeff Levin <jeff@levinology.com>
2024-09-09 10:06:18 -0800

    basics

---
+++ CI/api_test.ts
@@ -0,0 +1,15 @@
+import { check } from 'k6';
+import { http } from 'k6/http'
+
+export const options = {
+  scenarios: {
+    api: {
+      executor: 'shared-iterations',
+    },
+  },
+};
+
+export default function () {
+  const res = http.request('GET', 'http://localhost:3000');
+  check(res, { 'status ok': res.status === 200 });
+}
