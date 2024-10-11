debc2267d8f39261229b2eb6f660c47f34972a0a
Pablo Chacin <pablochacin@gmail.com>
2024-10-11 20:15:24 +0200

    WIP: session recorder
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

---
+++ pkg/recorder/recorder.go
@@ -0,0 +1,187 @@
+// Package recorder implements API call recording
+package recorder
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+	"time"
+)
+
+const (
+	DefaultProxyPort = 8080
+)
+
+var (
+	ErrInvalidOptions   = errors.New("invalid options")
+	ErrStartProxy       = errors.New("error starting proxy")
+	ErrStoppingProxy    = errors.New("error stopping proxy")
+	ErrParsingRecording = errors.New("error parsing recording")
+)
+
+type Request struct {
+	Host   string
+	Method string
+	Path   string
+	Status string
+}
+type Recording struct {
+	Requests []Request
+}
+
+// Recorder is an API call recorder
+type Recorder interface {
+	GetRecording() (Recording, error)
+}
+
+type ProxyRecorder struct {
+	url     string
+	cmd     *exec.Cmd
+	capture *bytes.Buffer
+}
+
+type ProxyOptions struct {
+	GracePeriod time.Duration
+	Scheme      string
+	Address     string
+	Port        int
+	Target      string
+	Verbose     bool
+}
+
+func NewProxyRecorder(opts ProxyOptions) (*ProxyRecorder, error) {
+	if opts.Target == "" {
+		return nil, fmt.Errorf("%w: target cannot be empty", ErrInvalidOptions)
+	}
+
+	targetUrl, err := url.Parse(opts.Target)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrInvalidOptions, err)
+	}
+
+	if opts.Port == 0 {
+		opts.Port = DefaultProxyPort
+	}
+
+	if opts.Scheme == "" {
+		opts.Scheme = "http"
+	}
+	if opts.Address == "" {
+		opts.Address = "127.0.0.1"
+	}
+
+	if opts.GracePeriod == 0 {
+		opts.GracePeriod = time.Second
+	}
+
+	args := []string{
+		"--listen-port",
+		fmt.Sprintf("%d", opts.Port),
+		"--ssl-insecure",
+		"--set", "flow_detail=1",
+		"--set", "termlog_verbosity=ERROR",
+		"--set", fmt.Sprintf("dumper-filter='~d %s'", targetUrl.Hostname()),
+	}
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd := exec.Command("/usr/bin/mitmdump", args...)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	if opts.Verbose {
+		cmd.Stdout = io.MultiWriter(stdout, os.Stdout)
+		cmd.Stderr = io.MultiWriter(stderr, os.Stderr)
+	}
+
+	if err = cmd.Start(); err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrStartProxy, err)
+	}
+
+	time.Sleep(opts.GracePeriod)
+	err = cmd.Process.Signal(syscall.Signal(0))
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrStartProxy, stderr.String())	
+	}
+
+	return &ProxyRecorder{
+		url:     fmt.Sprintf("%s://%s:%d", opts.Scheme, opts.Address, opts.Port),
+		cmd:     cmd,
+		capture: stdout,
+	}, nil
+}
+
+
+func (p *ProxyRecorder) GetRecording() (Recording, error) {
+	//stop the proxy
+	err := p.cmd.Process.Kill()
+	if err != nil {
+		return Recording{}, fmt.Errorf("%w: %s", ErrStoppingProxy, err)
+	}
+
+	return ParseRecording(p.capture.Bytes())
+}
+
+func (p *ProxyRecorder) Url() string {
+	return p.url
+}
+
+// ParseRecording parses the recording output and returns a Recording
+func ParseRecording(buffer []byte) (Recording, error) {
+	recording := Recording{}
+
+	scanner := bufio.NewScanner(bytes.NewReader(buffer))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		// assume the recording always starts with request line
+		// Example: 127.0.0.1:40288: POST https://instance.grafana.net/login HTTP/2.0
+		// the HTTP/<version> is optional
+		tokens := strings.Split(line, " ")
+		if len(tokens) < 3 {
+			return Recording{}, fmt.Errorf("%w: %s", ErrParsingRecording, line)
+		}
+
+		requestURL, err := url.Parse(tokens[2])
+		if err != nil {
+			return Recording{}, fmt.Errorf("%w: %s", ErrParsingRecording, line)
+		}
+		request := Request{
+			Host:   requestURL.Host,
+			Path:   requestURL.Path,
+			Method: tokens[1],
+		}
+
+		if scanner.Scan() {
+			// parse response (has leading spaces)
+			// Example:	<< HTTP/2.0 200 OK 65b
+			// the HTTP/<version> is optional
+
+			line = strings.TrimLeft(scanner.Text(), " ")
+			tokens = strings.Split(line, " ")
+			if len(tokens) < 3 {
+				return Recording{}, fmt.Errorf("%w: %s", ErrParsingRecording, line)
+			}
+			if tokens[0] != "<<" {
+				return Recording{}, fmt.Errorf("%w: %s", ErrParsingRecording, line)
+			}
+
+			if !strings.HasPrefix(tokens[1], "HTTP") {
+				request.Status = tokens[1]
+			} else {
+				request.Status = tokens[2]
+			}
+		}
+		recording.Requests = append(recording.Requests, request)
+	}
+	return recording, nil
+}
---
+++ pkg/recorder/recorder_test.go
@@ -0,0 +1,186 @@
+package recorder
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+const capture = `
+127.0.0.1:40288: GET https://instance.grafana.net/api/frontend/settings HTTP/2.0
+     << HTTP/2.0 200 OK 10.56k
+127.0.0.1:40298: POST https://instance.grafana.net/login HTTP/2.0
+     << HTTP/2.0 200 OK 41b
+127.0.0.1:40302: POST https://instance.grafana.net/api/dashboards/db HTTP/2.0
+     << HTTP/2.0 200 OK 175b
+127.0.0.1:40318: DELETE https://instance.grafana.net/api/dashboards/uid/jQzZSWJqLhLeoUv HTTP/2.0
+     << HTTP/2.0 200 OK 123b
+`
+
+func TestParseRecording(t *testing.T) {
+	testCases := []struct {
+		testCase  string
+		expectErr error
+		expect    Recording
+	}{
+		{
+			testCase: "valid recording",
+			expect: Recording{
+				Requests: []Request{
+					{
+						Host:   "instance.grafana.net",
+						Path:   "/api/frontend/settings",
+						Method: "GET",
+						Status: "200",
+					},
+					{
+						Host:   "instance.grafana.net",
+						Path:   "/login",
+						Method: "POST",
+						Status: "200",
+					},
+					{
+						Host:   "instance.grafana.net",
+						Path:   "/api/dashboards/db",
+						Method: "POST",
+						Status: "200",
+
+					},
+					{
+						Host:   "instance.grafana.net",
+						Path:   "/api/dashboards/uid/jQzZSWJqLhLeoUv",
+						Method: "DELETE",
+						Status: "200",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testCase, func(t *testing.T) {
+			recording, err := ParseRecording([]byte(capture))
+			if !errors.Is(err, tc.expectErr) {
+				t.Fatalf("expected error %v got %v", tc.expectErr, err)
+			}
+
+			if tc.expectErr != nil {
+				return
+			}
+
+			if !reflect.DeepEqual(recording, tc.expect) {
+				t.Fatalf("expected %v got %v", tc.expect, recording)
+			}
+		})
+	}
+}
+
+type flow struct {
+	path   string
+	method string
+	status int
+}
+
+func TestRecordingProxy(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		testCase  string
+		flows     []flow
+		recording Recording
+	}{
+		{
+			testCase: "valid recording",
+			flows: []flow{
+				{
+					path:   "/api/frontend/settings",
+					method: "GET",
+					status: 200,
+				},
+				{
+					path:   "/login",
+					method: "POST",
+					status: 200,
+				},
+				{
+					path:   "/api/dashboards/db",
+					method: "POST",
+					status: 200,
+				},
+			},
+			recording: Recording{
+				Requests: []Request{
+					{
+						Path:   "/api/frontend/settings",
+						Method: "GET",
+						Status: "200",
+					},
+					{
+						Path:   "/login",
+						Method: "POST",
+						Status: "200",
+					},
+					{
+						Path:   "/api/dashboards/db",
+						Method: "POST",
+						Status: "200",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.testCase, func(t *testing.T) {
+			// start an http server that returns 200 for all requests
+			target := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			}))
+			defer target.Close()
+
+			recorder, err := NewProxyRecorder(ProxyOptions{
+				Scheme: "http",
+				Target: target.URL,
+				Port: 8091,
+				Verbose: true,
+			})
+			if err != nil {
+				t.Fatalf("starting recorder: %v", err)
+			}
+
+			proxyUrl, err := url.Parse(recorder.Url())
+
+			client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+
+			for _, flow := range tc.flows {
+				request, _ := http.NewRequest(flow.method, target.URL+flow.path, nil)
+
+				resp, err := client.Do(request)
+				if err != nil {
+					t.Fatal("cannot send reque st", err)
+				}
+
+				if resp.StatusCode != flow.status {
+					t.Fatalf("expected status %v got %v", flow.status, resp.StatusCode)
+				}
+			}
+
+			recording, err := recorder.GetRecording()
+			if err != nil {
+				t.Fatalf("getting recording: %v", err)
+			}
+
+
+			for i, rec := range tc.recording.Requests {
+				// host can't be compared because depends on a random port
+				if recording.Requests[i].Method != rec.Method ||recording.Requests[i].Status != rec.Status {
+					t.Fatalf("expected %v got %v", tc.recording, recording)
+				}
+			}
+		})
+	}
+}
