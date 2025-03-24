cb20a96af09f1819f632bdfe4f293e29217199ae
Pablo Chacin <pablochacin@gmail.com>
2025-03-24 18:42:15 +0100

    fix service label
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/reporter/prometheus.go
+++ pkg/reporter/prometheus.go
@@ -71,7 +71,7 @@ func (p *PrometheusReporter) Report(
 	var ts []*prompb.TimeSeries
 
 	labels := map[string]string{
-		"service":         "grafana-bench",
+		"service":         "bench",
 		"grafana_version": suiteRun.GrafanaVersion,
 		"status":          string(summary.Status),
 		"suite_run":       suiteRun.Name,
