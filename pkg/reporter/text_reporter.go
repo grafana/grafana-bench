4f0ac2b5e8bc447209e6ae1f7d93bc41b6d43a14
Matheus Macabu <macabu.matheus@gmail.com>
2025-08-22 14:02:43 +0200

    TextReporter: Use tabwriter for prettier output

--- pkg/reporter/text_reporter.go
+++ pkg/reporter/text_reporter.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/grafana/grafana-bench/pkg/executor"
 )
@@ -24,21 +26,27 @@ func (r *TextReporter) Report(
 	_ executor.SuiteRun,
 	suiteRunSummary executor.SuiteRunSummary,
 ) error {
+	tw := tabwriter.NewWriter(r.report, 5, 0, 1, ' ', 0)
+	defer tw.Flush()
+
 	for _, testRun := range suiteRunSummary.TestRuns {
 		fmt.Fprintf(
-			r.report,
-			"%s/%s ... %s\n",
+			tw,
+			"[%s]\t%s\t%s/%s\n",
+			strings.ToUpper(string(testRun.Status)),
+			testRun.TotalDuration.String(),
 			testRun.TestFolder,
 			testRun.TestFile,
-			testRun.Status,
 		)
 	}
-	fmt.Fprintf(r.report, "\nTests executed %d\n", suiteRunSummary.TestsExecuted)
-	fmt.Fprintf(r.report, "Tests passed %d\n", suiteRunSummary.TestsPassed)
-	fmt.Fprintf(r.report, "Tests flaky %d\n", suiteRunSummary.TestsFlaky)
-	fmt.Fprintf(r.report, "Tests failed %d\n", suiteRunSummary.TestsFailed)
-	fmt.Fprintf(r.report, "Tests error %d\n", suiteRunSummary.TestsError)
-	fmt.Fprintf(r.report, "\nTests suite %s\n", suiteRunSummary.Status)
+
+	fmt.Fprintf(tw, "\n----------------SUMMARY----------------\n")
+	fmt.Fprintf(tw, "Executed:\t%d\n", suiteRunSummary.TestsExecuted)
+	fmt.Fprintf(tw, "Passed:\t%d\n", suiteRunSummary.TestsPassed)
+	fmt.Fprintf(tw, "Flaky:\t%d\n", suiteRunSummary.TestsFlaky)
+	fmt.Fprintf(tw, "Failed:\t%d\n", suiteRunSummary.TestsFailed)
+	fmt.Fprintf(tw, "Errors:\t%d\n", suiteRunSummary.TestsError)
+	fmt.Fprintf(tw, "Suite:\t%s\n", suiteRunSummary.Status)
 
 	return nil
 }
