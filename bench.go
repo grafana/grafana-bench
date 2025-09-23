7f9b1f1875042d0ccbca367194651d40196ea840
Jeff Levin <jeff.levin@grafana.com>
2025-09-23 12:02:37 -0800

    add separate exit code for test command (#694)

--- bench.go
+++ bench.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"os"
 
 	"github.com/grafana/grafana-bench/cmd/root"
+	testcmd "github.com/grafana/grafana-bench/cmd/test"
 	"github.com/grafana/grafana-bench/pkg/utils/logger"
 )
 
@@ -16,6 +18,13 @@ func main() {
 	err := root.ExecuteContext(context.Background())
 	if err != nil {
 		log.Error(err.Error())
-		os.Exit(1)
+
+		// Handle custom exit codes for test command
+		var testFailureErr testcmd.TestFailureError
+		if errors.As(err, &testFailureErr) {
+			os.Exit(1) // Test suite failed
+		} else {
+			os.Exit(2) // All other errors
+		}
 	}
 }
